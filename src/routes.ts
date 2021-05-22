d8c62fb2b8a65c0f66d2845ec283a48d098d5ac0
Paul Zadorozhnyi <pavlo.zadorozhnyi@yalantis.com>
2021-05-22 15:32:19 +0300

    First 3d party call

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
+import axios from 'axios'
 
 const routes = Router()
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' })
+routes.get('/vacancy/search', async (req, res) => {
+  const result = await axios.get('https://api.rabota.ua/vacancy/search', {
+    params: req.query,
+  })
+
+  return res.json(result.data)
 })
 
 export default routes
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
 import app from './app'
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`App has been started on localhost:${process.env.PORT || 5000}`)
+})
